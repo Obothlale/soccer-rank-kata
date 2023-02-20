9f4ee20a9c076ac7d1c12c194da09c02e63c31a0
Obothlale Kganane <obothlale.kganane@gmail.com>
2023-02-20 20:46:32 +0200

    * Run application jar via command line with path to file argument

---
+++ README.txt
@@ -0,0 +1,3 @@
+To execute application run as follows:
+
+java -jar target/soccer-rank-kata.jar /path/to/file/with/testData.txt
--- src/main/java/com/soccer/Application.java
+++ src/main/java/com/soccer/Application.java
@@ -1,8 +1,36 @@
 package com.soccer;
 
+import com.soccer.kata.service.GamesServices;
+import com.soccer.kata.service.impl.GamesServicesImpl;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
+        if (args == null || args.length == 0) {
+            System.err.println("Application requires single '/path/file' containing match data as arguments");
+            return;
+        }
+        try {
+            FileReader fileReader = new FileReader(args[0]);
+            Scanner scanner = new Scanner(fileReader);
+            String input = getInput(scanner);
+            GamesServices gamesServices = new GamesServicesImpl();
+            String rank = gamesServices.rank(input);
+            System.out.println(rank);
+            fileReader.close();
+        } catch (Exception fileNotFoundException) {
+            System.err.println("Unable to read file:" + args[0] + "\n With Exception:\n" + fileNotFoundException.getCause());
+        }
+    }
 
+    private static String getInput(Scanner scanner) {
+        StringBuilder input = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            input.append(scanner.nextLine()).append("\n");
+        }
+        return input.toString();
     }
 }

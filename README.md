c4168a42f0d1d882b7887e1e1573df97405630eb
QXZ14RZ <obothlale.kganane@partner.bmw.co.za>
2023-02-23 16:36:32 +0200

    * Run application on interactive mode as you type input as well

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 - Pre-build target/soccer-rank-kata.jar file included in this repo
 
 - To execute application run as follows:
-  java -jar target/soccer-rank-kata.jar /path/to/file/with/testData.txt
+
+  1. Pass in a file as arguments
+    java -jar target/soccer-rank-kata.jar /path/to/file/with/testData.txt
+  
+  2. Interactive mode
+    java -jar target/soccer-rank-kata.jar -it
--- src/main/java/com/soccer/Application.java
+++ src/main/java/com/soccer/Application.java
@@ -10,21 +10,53 @@ public class Application {
 
     public static void main(String[] args) {
         if (args == null || args.length == 0) {
-            System.err.println("Application requires single '/path/file' containing match data as arguments");
+            System.err.println("Application requires single '/path/file' containing match data as arguments or -it to run in interactive mode.");
             return;
         }
+        if(args[0].equals("-it")){
+            runInInteractiveMode();
+            return;
+        }
+        passInFileAsArguments(args[0]);
+    }
+
+    private static void passInFileAsArguments(String arg) {
         try {
-            FileReader fileReader = new FileReader(args[0]);
+            FileReader fileReader = new FileReader(arg);
             Scanner scanner = new Scanner(fileReader);
             String input = getInput(scanner);
             GamesServices gamesServices = new GamesServicesImpl();
             System.out.println("\n"+gamesServices.rank(input));
             fileReader.close();
         } catch (Exception exception) {
-            throw new RuntimeException("Unable to read file: " + args[0], exception);
+            throw new RuntimeException("Unable to read file: " + arg, exception);
         }
     }
 
+    private static void runInInteractiveMode() {
+        Scanner scanner = new Scanner(System.in);
+        GamesServices gamesServices = new GamesServicesImpl();
+        printOptions();
+        while(scanner.hasNext()){
+            try {
+                String input = scanner.nextLine();
+                if (input.equals("exit")) {
+                    break;
+                }
+                System.out.println("\n===New Rank===");
+                System.out.println(gamesServices.rank(input));
+            } catch (Exception exception) {
+                System.err.println("Acceptable input examples as follows.\nExample single line:\nLions 3, Snakes 3\n");
+            }
+        }
+    }
+
+    private static void printOptions() {
+        System.out.println("\n\n\n===Options===");
+        System.out.println("1. Type exit + enter to exit program");
+        System.out.println("2. Type a match to enter a new rank in the format: <team1 name> <score>, <team2 name> <score>.\nExample:\nLions 3, Snakes 3\n" +
+                "Tarantulas 1, FC Awesome 0\n");
+    }
     private static String getInput(Scanner scanner) {
         StringBuilder input = new StringBuilder();
         while (scanner.hasNextLine()) {
